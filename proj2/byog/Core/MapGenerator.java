0ac4b2e38ea4cf45ba7a22421f1c5bb34a00aed3
404ARE <936233544@qq.com>
2022-09-28 23:55:33 +0800

    proj2 submit 2: working on solving wall conflict

--- proj2/byog/Core/MapGenerator.java
+++ proj2/byog/Core/MapGenerator.java
@@ -6,21 +6,27 @@ import byog.TileEngine.Tileset;
 
 import javax.sql.rowset.serial.SQLOutputImpl;
 import java.awt.*;
+import java.util.IllegalFormatWidthException;
 import java.util.LinkedList;
 import java.util.Random;
 
 /** This class generates a world to verify the random algorithm. */
 public class MapGenerator {
-    private static final int WIDTH = 60;
-    private static final int HEIGHT = 40;
     private static Random RANDOM;
+    private static int WIDTH;
+    private static int HEIGHT;
+    private static final int BLOCK_WIDTH = 10;
+    private static final int BLOCK_HEIGHT = 10;
 
     private static class Room {
-        private static Point pos;
-        private static int length;
-        private static int width;
-        private static final int MAX_WIDTH = 8;
-        private static final int MAX_HEIGHT = 8;
+        private Point pos;
+        private int length;
+        private int width;
+        private static final int MAX_WIDTH = 12;
+        private static final int MIN_WIDTH = 4;
+        private static final int MAX_HEIGHT = 12;
+        private static final int MIN_HEIGHT = 4;
+        private static final int ROOM_NUM = 50;
         private static Random random;
 
         public Room() {
@@ -55,77 +61,142 @@ public class MapGenerator {
     }
 
     public TETile[][] generateMap(int width, int height) {
+        WIDTH = width;
+        HEIGHT = height;
         //Initialize
         TETile[][] world = new TETile[width][height];
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 world[i][j] = Tileset.GRASS;
             }
         }
 
-        //generate room list
-        LinkedList<Room> roomL = generateRoomList();
-        System.out.println(roomL);
-        Room r = new Room(new Point(4, 5), 3, 4);
-        roomL.add(r);
-        //generate hallway list
-        LinkedList<Room> hallWayL = generateHallWayList();
-        //place room and hallway
-        for (Room room : roomL) {
-            drawRoom(world, room);
+        LinkedList<Point> points = computeBlockPoint(world);
+        for (Point p: points) {
+            drawBlock(world, p);
         }
-        //handle overlapped
+//
+//        //generate and draw room list
+//        int roomNum = RandomUtils.uniform(RANDOM, Room.ROOM_NUM);
+//        System.out.println("roomNum:" + roomNum);
+//        LinkedList<Room> roomList = new LinkedList<>();
+//        for (int i = 0; i < roomNum; ++i) {
+//            int posX = RandomUtils.uniform(RANDOM, width);
+//            int posY = RandomUtils.uniform(RANDOM, height);
+//            int w = RandomUtils.uniform(RANDOM, Room.MIN_WIDTH, Room.MAX_WIDTH);
+//            int h = RandomUtils.uniform(RANDOM, Room.MIN_HEIGHT, Room.MAX_HEIGHT);
+//            System.out.println("" + posX + posY + w + h);
+//            drawRoom(world, new Room(new Point(posX, posY), w, h));
+//        }
+//
+//        //generate and draw hallway
+//        int hallwayNum = RANDOM.nextInt();
+//        for (int i = 0; i < hallwayNum; ++i) {
+//            int posX = RandomUtils.uniform(RANDOM, width);
+//            int posY = RandomUtils.uniform(RANDOM, height);
+//            int w = 1;
+//            int h = 1;
+//            if (RandomUtils.bernoulli(RANDOM)) {
+//                h = RandomUtils.uniform(RANDOM, Room.MAX_HEIGHT);
+//            } else {
+//                w = RandomUtils.uniform(RANDOM, Room.MAX_WIDTH);
+//            }
+//            Point p = new Point(posX, posY);
+//            Room r = new Room(p, w, h);
+//            drawRoom(world, r);
+//        }
 
         TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
+        ter.initialize(width, height);
         ter.renderFrame(world);
 
         return world;
     }
 
-    private LinkedList<Room> generateRoomList() {
-        LinkedList<Room> roomList = new LinkedList<>();
-        int roomNum = RandomUtils.uniform(RANDOM, 20);
-        for (int i = 0; i < roomNum; ++i) {
-            int posX = RandomUtils.uniform(RANDOM, WIDTH);
-            int posY = RandomUtils.uniform(RANDOM, HEIGHT);
-            int w = RandomUtils.uniform(RANDOM, Room.MAX_WIDTH);
-            int h = RandomUtils.uniform(RANDOM, Room.MAX_HEIGHT);
-            System.out.println("" + posX + posY + w + h);
-            roomList.add(new Room(new Point(posX, posY), w, h));
-        }
-        return roomList;
-    }
-
-    private LinkedList<Room> generateHallWayList() {
-        int hallwayNum = RANDOM.nextInt();
-        LinkedList<Room> hallwayList = new LinkedList<>();
-        for (int i = 0; i < hallwayNum; ++i) {
-            int posX = RandomUtils.uniform(RANDOM, WIDTH);
-            int posY = RandomUtils.uniform(RANDOM, HEIGHT);
-            int w = 1;
-            int h = 1;
-            if (RandomUtils.bernoulli(RANDOM)) {
-                h = RandomUtils.uniform(RANDOM, Room.MAX_HEIGHT);
-            } else {
-                w = RandomUtils.uniform(RANDOM, Room.MAX_WIDTH);
-            }
-            Point p = new Point(posX, posY);
-            Room r = new Room(p, w, h);
-            hallwayList.add(r);
-        }
-        return hallwayList;
-    }
+//    private LinkedList<Room> generateHallWayList() {
+//        int hallwayNum = RANDOM.nextInt();
+//        LinkedList<Room> hallwayList = new LinkedList<>();
+//        for (int i = 0; i < hallwayNum; ++i) {
+//            int posX = RandomUtils.uniform(RANDOM, );
+//            int posY = RandomUtils.uniform(RANDOM, HEIGHT);
+//            int w = 1;
+//            int h = 1;
+//            if (RandomUtils.bernoulli(RANDOM)) {
+//                h = RandomUtils.uniform(RANDOM, Room.MAX_HEIGHT);
+//            } else {
+//                w = RandomUtils.uniform(RANDOM, Room.MAX_WIDTH);
+//            }
+//            Point p = new Point(posX, posY);
+//            Room r = new Room(p, w, h);
+//            hallwayList.add(r);
+//        }
+//        return hallwayList;
+//    }
 
     private void drawRoom(TETile[][] world, Room room) {
+        int BottomLeftX = room.getPos().x;
+        int BottomLeftY = room.getPos().y;
+        int width = room.getWidth();
+        int length = room.getLength();
+        int topRightX = BottomLeftX + width;
+        int topRightY = BottomLeftY + length;
+        if (topRightX > WIDTH || topRightY > HEIGHT) {
+            return;
+        }
         for (int i = 0; i < room.getWidth(); ++i) {
             for (int j = 0; j < room.getLength(); ++j) {
-                if (i == 0 || j == 0) {
-                    world[room.getPos().x + i][room.getPos().y + j] = Tileset.WALL;
+                int posX = room.getPos().x + i;
+                int posY = room.getPos().y + j;
+                boolean blockEdge = i == 0 || j == 0 || i == width - 1 || j == length - 1;
+                boolean publicWall = (world[posX - 1][posY].equals(Tileset.FLOOR)
+                                    && world[posX + 1][posY].equals(Tileset.FLOOR))
+                                    || (world[posX][posY - 1].equals(Tileset.FLOOR)
+                                    && world[posX][posY + 1].equals(Tileset.FLOOR));
+                if (blockEdge) {
+                    if (world[posX][posY].equals(Tileset.WALL) && !(posX == 0 || posY == 0 || posX == WIDTH - 1 || posY == HEIGHT - 1) && publicWall) {
+                        world[posX][posY] = Tileset.FLOOR;
+                    }
+                    if (!world[posX][posY].equals(Tileset.FLOOR)) {
+                        world[posX][posY] = Tileset.WALL;
+                    }
                 } else {
-                    world[room.getPos().x + i][room.getPos().y + j] = Tileset.FLOOR;
+                    world[posX][posY] = Tileset.FLOOR;
                 }
             }
+        }
+    }
+
+    private LinkedList<Point> computeBlockPoint(TETile[][] world) {
+        LinkedList<Point> points = new LinkedList<>();
+        int pieceNumX = WIDTH / BLOCK_WIDTH;
+        int pieceNumY = HEIGHT / BLOCK_HEIGHT;
+        for (int i = 0; i < pieceNumX; i++) {
+            for (int j = 0; j < pieceNumY; j++) {
+                points.add(new Point(i * BLOCK_WIDTH, j * BLOCK_HEIGHT));
+                world[i * BLOCK_WIDTH][j * BLOCK_HEIGHT] = Tileset.TREE;
+            }
+        }
+        System.out.println(points);
+        return points;
+    }
+
+    private void drawBlock(TETile[][] world, Point point) {
+        //draw a room in this block with probability 0.7
+        if (RandomUtils.bernoulli(RANDOM, 0.9)) {
+            //0.7 chance point locate in left down corner
+            int posX;
+            int posY;
+            if (RandomUtils.bernoulli(RANDOM, 0.7)) {
+                posX = point.x + RandomUtils.uniform(RANDOM, BLOCK_WIDTH / 2);
+                posY = point.y + RandomUtils.uniform(RANDOM, BLOCK_HEIGHT / 2);
+            } else {
+                posX = point.x + RandomUtils.uniform(RANDOM, BLOCK_WIDTH);
+                posY = point.y + RandomUtils.uniform(RANDOM, BLOCK_HEIGHT);
+            }
+
+            int width = RandomUtils.uniform(RANDOM, Room.MIN_WIDTH, Room.MAX_WIDTH);
+            int height = RandomUtils.uniform(RANDOM, Room.MIN_HEIGHT, Room.MAX_HEIGHT);
+            drawRoom(world, new Room(new Point(posX, posY), width, height));
         }
     }
 }
